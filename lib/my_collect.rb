6e2342444cc8c0913fdbd0fe21364643133e2200
Brendan R Borgers <brendanborgers@gmail.com>
2019-12-16 22:36:12 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,14 @@
+def my_collect(words) 
+  i = 0 
+  names = []
+  
+  while i < words.size
+  name.push yield words[i]
+  i =i + 1
 
-
+  end 
+  words
+else
+  return "Hey! No block was given!"
+end
+end
